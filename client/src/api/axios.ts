e2013dda069748606677cbfb1f985a591685d01c
Anvar <anvarmusa12@gmail.com>
2023-09-26 12:38:45 +0200

    axios private added

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -3,3 +3,9 @@ import axios from "axios";
 export const axiosInstance = axios.create({
     baseURL: "http://localhost:5050/",
 });
+
+export const axiosPrivate = axios.create({
+    baseURL: "http://localhost:5050/",
+    headers: { "Content-Type": "application/json" },
+    withCredentials: true,
+});
--- client/src/pages/Courses.tsx
+++ client/src/pages/Courses.tsx
@@ -1,9 +1,30 @@
+import { useState, useEffect } from "react";
+import { axiosInstance } from "../api/axios";
+import { useLocation } from "react-router-dom";
+import useAxiosPrivate from "../hooks/useAxiosPrivate";
+
 const Courses = () => {
+    // const location = useLocation();
+    const [coursesArr, setCoursesArr] = useState([]);
+    const getCourses = async () => {
+        try {
+            const resp = await axiosPrivate.get("/courses");
+            console.log(resp);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    const axiosPrivate = useAxiosPrivate();
+
+    useEffect(() => {
+        getCourses();
+    }, [location?.pathname]);
+
     return (
-        <section>
+        <section className=" pt-24">
             <div className="max-w-5xl px-4 m-auto flex justify-between items-center">
-                <h1 className="font-bold text-4xl">Your courses</h1>
-                courses here
+                <h1 className="font-bold text-5xl">Your courses</h1>
+                <div>courses here</div>
             </div>
         </section>
     );
