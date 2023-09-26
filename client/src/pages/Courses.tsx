175e95d9b2ec8bd1a0ace3a73136a506ec584bc2
Anvar <anvarmusa12@gmail.com>
2023-09-26 13:05:18 +0200

    basic awfull styles

--- client/src/pages/Courses.tsx
+++ client/src/pages/Courses.tsx
@@ -34,8 +34,8 @@ const Courses = () => {
             setCoursesEls(() => {
                 return coursesArr.map((course) => {
                     return (
-                        <Link to={`/${course.slug}`} key={course._id}>
-                            <h3 className="">{course.title}</h3>
+                        <Link to={`/${course.slug}`} key={course._id} className="border-2 border-primary rounded-md bg-gray-100 py-2 px-4 block">
+                            <h3 className="font-bold text-2xl">{course.title}</h3>
                             <p>{course.description}</p>
                         </Link>
                     );
@@ -48,7 +48,7 @@ const Courses = () => {
         <section className=" pt-24">
             <div className="max-w-5xl px-4 m-auto">
                 <h1 className="font-bold text-5xl">Your courses</h1>
-                <div className="mt-8">{coursesEls}</div>
+                <div className="mt-8 gap-2 flex flex-col">{coursesEls}</div>
             </div>
         </section>
     );
