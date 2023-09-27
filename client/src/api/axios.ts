2fcd5ac3ca0f79e5bf1d09eefddbe42df383b111
Anvar <anvarmusa12@gmail.com>
2023-09-27 16:52:32 +0200

    env variables added to vite

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,11 +1,13 @@
 import axios from "axios";
 
+const SERVER_BASE_URL = import.meta.env.VITE_SERVER_API_URL;
+
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5050/",
+    baseURL: SERVER_BASE_URL,
 });
 
 export const axiosPrivate = axios.create({
-    baseURL: "http://localhost:5050/",
+    baseURL: SERVER_BASE_URL,
     headers: { "Content-Type": "application/json" },
     withCredentials: true,
 });
