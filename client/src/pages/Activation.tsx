3f3d107602d658ba9bbf17659ffa726cf6244848
Anvar <anvarmusa27@gmail.com>
2023-10-07 20:05:14 +0200

    activation page redirect if user is active

--- client/src/pages/Activation.tsx
+++ client/src/pages/Activation.tsx
@@ -1,13 +1,21 @@
 import { IconMailOpened } from "@tabler/icons-react";
+import useAuth from "../hooks/useAuth";
+import { Navigate } from "react-router-dom";
 
 const Activation = () => {
+    const { auth } = useAuth();
+
+    if (auth?.isActive) return <Navigate to={"/courses"} />;
+
     return (
         <section className="py-20">
             <div className="max-w-5xl px-4 m-auto">
                 <h1 className="font-bold text-5xl flex items-center gap-2">
                     Email verification required <IconMailOpened size={40} />
                 </h1>
-                <p className="text-2xl mt-4">We've sent an email with a verification link.</p>
+                <p className="text-2xl mt-4">
+                    We've sent an email with a verification link.
+                </p>
             </div>
         </section>
     );
