edfd6da343095c9ca73a0c33562ebfadb7f8503c
Anvar <anvarmusa27@gmail.com>
2023-10-21 12:36:25 +0200

    show user type

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -14,18 +14,21 @@ const Dashboard = () => {
             console.log(err);
         }
     };
-
+    
     return (
         <section className="pt-32 pb-28">
             <div className="max-w-5xl px-4 mx-auto">
                 <h1 className="font-bold text-4xl">Your dashboard</h1>
                 {auth?.accessToken ? (
-                    <button
-                        className="mt-4 bg-secRed inline-block text-white py-2 px-8 rounded-full font-semibold hover:shadow-lg transition-all"
-                        onClick={handleLogout}
-                    >
-                        Log out
-                    </button>
+                    <>
+                        <p>You are a {auth?.userType}</p>
+                        <button
+                            className="mt-4 bg-secRed inline-block text-white py-2 px-8 rounded-full font-semibold hover:shadow-lg transition-all"
+                            onClick={handleLogout}
+                        >
+                            Log out
+                        </button>
+                    </>
                 ) : (
                     ""
                 )}
