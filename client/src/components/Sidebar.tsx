c1f228f626d343737d5794826422feb07adc93fe
Anvar <anvarmusa27@gmail.com>
2023-10-22 10:15:06 +0200

    menus json added

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
+import menus from "./../menus.json";
 
 const Sidebar = () => {
     const { auth } = useAuth();
@@ -8,38 +9,32 @@ const Sidebar = () => {
         <aside className="bg-input py-5 left-0 top-0 h-screen w-1/5">
             <h2 className="font-semibold text-xl px-4">{auth?.email}</h2>
             <h3 className="font-medium text-lg px-4">{auth?.userType}</h3>
-            <nav>
-                <ul>
-                    <li>
-                        <NavLink
-                            to={"/studio"}
-                            className={({ isActive }) =>
-                                `w-full block py-1 px-4 text-primText hover:bg-gray-300 ${
-                                    isActive
-                                        ? "font-bold bg-secRed sm:bg-transparent"
-                                        : "bg-primary sm:bg-transparent"
-                                }`
-                            }
-                        >
-                            Home
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink
-                            to={"/courses"}
-                            className={({ isActive }) =>
-                                `w-full block py-1 px-4 text-primText hover:bg-gray-300 ${
-                                    isActive
-                                        ? "font-bold bg-secRed sm:bg-transparent"
-                                        : "bg-primary sm:bg-transparent"
-                                }`
-                            }
-                        >
-                            Courses
-                        </NavLink>
-                    </li>
-                </ul>
-            </nav>
+            {menus?.sidebar ? (
+                <nav>
+                    <ul>
+                        {menus?.sidebar.map((menuItem) => {
+                            return (
+                                <li key={menuItem.id}>
+                                    <NavLink
+                                        to={menuItem.to}
+                                        className={({ isActive }) =>
+                                            `w-full block py-1 px-4 text-primText hover:bg-gray-300 ${
+                                                isActive
+                                                    ? "font-bold bg-secRed sm:bg-transparent"
+                                                    : "bg-primary sm:bg-transparent"
+                                            }`
+                                        }
+                                    >
+                                        {menuItem.title}
+                                    </NavLink>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </nav>
+            ) : (
+                ""
+            )}
         </aside>
     );
 };
